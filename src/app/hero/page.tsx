562fe15e6c788daf79f11eba329996396990820c
TommyRizaldi <ach.rizaldiutomo@gmail.com>
2024-08-05 22:34:58 +0700

    first init

---
+++ src/app/hero/page.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return (
+    <section className="bg-gray-800 text-white py-20">
+      <div className="container mx-auto px-4 text-center">
+        <h2 className="text-5xl font-bold mb-4">Microcoft</h2>
+        <p className="text-lg mb-6">The Apel Competitor.</p>
+        <a href="#contact" className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700">Click It</a>
+      </div>
+    </section>
+    )
+}
